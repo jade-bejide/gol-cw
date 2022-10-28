6a6b9e23a6241bd08d62f1b43d8a05151d748e3e
Jadesola Bejide <vm21106@bristol.ac.uk>
2022-10-28 17:01:35 +0100

    Setting up single threaded sol

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,5 +1,10 @@
 package gol
 
+import (
+	"uk.ac.bris.cs/gameoflife/util"
+	"fmt"
+	"strconv"
+)
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
@@ -9,18 +14,144 @@ type distributorChannels struct {
 	ioInput    <-chan uint8
 }
 
+func countLiveNeighbours(p Params, x int, y int, world [][]byte) int {
+	liveNeighbours := 0
+
+
+	w := p.ImageWidth - 1
+	h := p.ImageHeight - 1
+
+	l := x - 1
+	r := x + 1
+	u := y + 1
+	d := y - 1
+
+	if l < 0 { l = w }
+	if r > w { r = 0 }
+	if u > h { u = 0 }
+	if d < 0 { d = h }
+
+
+	if world[u][x] == 255 { liveNeighbours += 1 }
+	if world[d][x] == 255 { liveNeighbours += 1 }
+	if world[u][l] == 255 { liveNeighbours += 1 }
+	if world[u][r] == 255 { liveNeighbours += 1 }
+	if world[d][l] == 255 { liveNeighbours += 1 }
+	if world[d][r] == 255 { liveNeighbours += 1 }
+	if world[y][l] == 255 { liveNeighbours += 1 }
+	if world[y][r] == 255 { liveNeighbours += 1 }
+
+	return liveNeighbours
+}
+
+func updateState(isAlive bool, neighbours int) bool {
+	return isAlive && neighbours > 1 && neighbours < 4 || !isAlive && neighbours == 3
+}
+
+func isAlive(x int, y int, world [][]byte) bool {
+	return world[y][x] != 0
+}
+
+func saveWorld(world [][]byte) [][]byte {
+	cp := make([][]byte, len(world))
+
+	for i := range world {
+		cp[i] = make([]byte, len(world[i]))
+		copy(cp[i], world[i])
+	}
+
+	return cp
+}
+
+func calculateNextState(p Params, world [][]byte) [][]byte {
+	x := 0
+	y := 0
+
+	worldCpy := saveWorld(world) //enables you to save what the last tick of the world was
+
+	for x < p.ImageWidth {
+		y = 0
+		for y < p.ImageHeight {
+			neighbours := countLiveNeighbours(p, x, y, world)
+			alive := isAlive(x, y, world)
+
+			alive = updateState(alive, neighbours)
+
+			if alive { worldCpy[y][x] = 255 } else { worldCpy[y][x] = 0 }
+
+			y += 1
+		}
+		x += 1
+	}
+
+	return worldCpy
+}
+
+func calculateAliveCells(p Params, world [][]byte) []util.Cell {
+	x := 0
+	y := 0
+
+	var cells []util.Cell
+
+	for x < p.ImageWidth {
+		y = 0
+		for y < p.ImageHeight {
+			if isAlive(x, y, world) {
+				c:= util.Cell{X: x,Y: y}
+				cells = append(cells, c)
+			}
+			y += 1
+		}
+		x += 1
+	}
+
+	return cells
+}
+
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
-
 	// TODO: Create a 2D slice to store the world.
+
+	c.ioCommand <- ioInput
+	filename := ""
+	wStr := strconv.Itoa(p.ImageWidth)
+	hStr := strconv.Itoa(p.ImageHeight)
+	filename += wStr
+	filename += "x"
+	filename += hStr
+
+	c.ioFilename <- filename
+
+    world := [][]byte{}
+    fmt.Println("here", c.ioFilename)
+    for y := 0; y < p.ImageHeight; y++ {
+        row := make([]byte, p.ImageWidth)
+
+        for x := 0; x < p.ImageWidth; x++ {
+            pixel := <-c.ioInput
+            row = append(row, pixel)
+        }
+
+
+        world = append(world, row)
+    }
+
+    fmt.Println(world)
 
 	turn := 0
 
 
 	// TODO: Execute all turns of the Game of Life.
 
+    for turn = 0; turn < p.Turns; turn++ {
+        world = calculateNextState(p, world)
+    }
 	// TODO: Report the final state using FinalTurnCompleteEvent.
 
+	aliveCells := calculateAliveCells(p, world)
+    final := FinalTurnComplete{CompletedTurns: p.Turns, Alive: aliveCells}
+
+    c.events <- final
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
--- gol/gol.go
+++ gol/gol.go
@@ -15,13 +15,16 @@ func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
 
 	ioCommand := make(chan ioCommand)
 	ioIdle := make(chan bool)
+	ioFilename := make(chan string)
+	ioOutput := make(chan uint8)
+	ioInput := make(chan uint8)
 
 	ioChannels := ioChannels{
 		command:  ioCommand,
 		idle:     ioIdle,
-		filename: nil,
-		output:   nil,
-		input:    nil,
+		filename: ioFilename,
+		output:   ioOutput,
+		input:    ioInput,
 	}
 	go startIo(p, ioChannels)
 
@@ -29,9 +32,9 @@ func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
 		events:     events,
 		ioCommand:  ioCommand,
 		ioIdle:     ioIdle,
-		ioFilename: nil,
-		ioOutput:   nil,
-		ioInput:    nil,
+		ioFilename: ioFilename,
+		ioOutput:   ioOutput,
+		ioInput:    ioInput,
 	}
 	distributor(p, distributorChannels)
 }
